d3ff322a50465c7ff062a3028ba95ef655641459
wcsharpe <80623631+wcsharpe@users.noreply.github.com>
2023-07-18 00:02:47 -0500

    mongodb interactivity

--- route/pages.js
+++ route/pages.js
@@ -1,25 +1,41 @@
 //where all ejs routing is happening
 const express = require('express');
 const router = express.Router();
+const Contact = require('./models/contact')
 
 router.get('/', (req,res)=>{
   res.render('bootstrapTemplate')
 })
 
-router.get('./contact', (req,res)=>{
+router.get('/contact', (req,res)=>{
   res.render('contact')
 })
 
-router.get('./education', (req,res)=>{
+router.get('/education', (req,res)=>{
   res.render('education')
 })
 
-router.get('./experience', (req,res)=>{
+router.get('/experience', (req,res)=>{
   res.render('experience')
 })
 
 router.get('/projects', (req,res)=>{
   res.render('projects')
+})
+
+router.post('/submitContact',(req,res)=>{
+  const contact = new Contact({
+    fname: req.body.fname,
+    lname: req.body.lname,
+    email: req.body.email,
+    comment: req.body.comment,
+    urgency: req.body.urgency
+  });
+  Contact.collection.insertOne(contact)
+  .then(result =>{
+    res.render('contact')
+  })
+  .catch(err => console.log(err));
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const route = require('./route/pages') // how pages will interact with each other
+const route = require('./route/pages'); // how pages will interact with each other
+const path = require('path');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,9 +14,9 @@ app.use('/', route);
 
 mongoose.connect('mongodb+srv://willcsharpe:mdNmPUeYCq1GrddO@cluster0.enrfnwi.mongodb.net/portfolioDB', {useNewURLParser: true, useUnifiedTopology: true})
 .then(()=> {
-  app.listen(3000, () =>{
-    console.log("server is running on 3000")
-  })
+  app.listen(port, () =>{
+    console.log(`Server is running on ${port}`)
+  });
 })
 
 
